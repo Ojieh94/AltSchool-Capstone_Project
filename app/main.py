befd795bb765d58102c6553e31abca6128500461
James0711 <jengreat94@gmail.com>
2024-08-07 06:16:36 +0100

    added routers and tests folder

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import user, auth, movie, rating, comment
+from .routers import ratings, user, auth, movie, comment
 from . import models
 from .database import engine 
 
@@ -21,5 +21,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(movie.router)
-app.include_router(rating.router)
+app.include_router(ratings.router)
 app.include_router(comment.router)
