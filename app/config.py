13e550d25f1c041b18896731af14e89802cbce0b
James0711 <jengreat94@gmail.com>
2024-08-08 05:28:13 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
+    """
+    A class for managing application settings using Pydantic's BaseSettings.
+    
+    This class loads settings from environment variables defined in the .env file.
+    """
     database_hostname: str
     database_port: str
     database_password: str
@@ -11,10 +15,12 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
- 
-    class Config:
-        env_file = ".env"
 
+    class Config:
+        """
+        Configuration for the Pydantic Settings class.
+        """
+        env_file = ".env"  # Path to the environment file from which to load variables
+
+# Instantiate the Settings class to access the configuration values
 settings = Settings()
- 
- 
