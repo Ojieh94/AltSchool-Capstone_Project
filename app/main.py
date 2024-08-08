846432c364528c4beb6fc7e5c5f5b3a80dc0cc2e
James0711 <jengreat94@gmail.com>
2024-08-08 05:03:21 +0100

    added comments

--- app/main.py
+++ app/main.py
@@ -1,25 +1,31 @@
+# Import necessary modules and classes from FastAPI and other components
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import ratings, user, auth, movie, comment
 from . import models
 from .database import engine 
 
+# Create an instance of the FastAPI class
 app = FastAPI()
 
-origins = ["*"]
+# Define CORS (Cross-Origin Resource Sharing) settings
+origins = ["*"]  # Allow requests from any origin
 
+# Add CORS middleware to the FastAPI app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,  # Specify the allowed origins (here, all origins are allowed)
+    allow_credentials=True,  # Allow credentials to be included in requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
+# Create all tables in the database based on the models defined
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(user.router)
-app.include_router(auth.router)
-app.include_router(movie.router)
-app.include_router(ratings.router)
-app.include_router(comment.router)
+# Include routers for different parts of the application
+app.include_router(user.router)  # User-related endpoints
+app.include_router(auth.router)  # Authentication-related endpoints
+app.include_router(movie.router)  # Movie-related endpoints
+app.include_router(ratings.router)  # Ratings-related endpoints
+app.include_router(comment.router)  # Comment-related endpoints
