30ba39ea6820cce71bd6850cd49af4eb5bf5e3c6
James0711 <jengreat94@gmail.com>
2024-08-09 03:14:40 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,23 +9,23 @@ from .database import engine
 app = FastAPI()
 
 # Define CORS (Cross-Origin Resource Sharing) settings
-origins = ["*"]  # Allow requests from any origin
+origins = ["*"] 
 
 # Add CORS middleware to the FastAPI app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Specify the allowed origins (here, all origins are allowed)
-    allow_credentials=True,  # Allow credentials to be included in requests
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 # Create all tables in the database based on the models defined
 models.Base.metadata.create_all(bind=engine)
 
 # Include routers for different parts of the application
-app.include_router(user.router)  # User-related endpoints
-app.include_router(auth.router)  # Authentication-related endpoints
-app.include_router(movie.router)  # Movie-related endpoints
-app.include_router(ratings.router)  # Ratings-related endpoints
-app.include_router(comment.router)  # Comment-related endpoints
+app.include_router(user.router) 
+app.include_router(auth.router)  
+app.include_router(movie.router)  
+app.include_router(ratings.router)  
+app.include_router(comment.router)  
